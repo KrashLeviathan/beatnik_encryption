3fd4d987f968cc2c287c085ce4351f8a99f7217b
KrashLeviathan <nathankarasch@gmail.com>
2016-10-19 11:05:27 -0500

    Refactored index.html and wrote TODO's in beatnik_encryption.js

---
+++ beatnik_encryption.js
@@ -0,0 +1,20 @@
+// COLLECT WORDS
+
+// TODO: Find a dictionary word API
+// TODO: Use word API to get N words for each possible word score needed, and populate to score:word[] map
+
+// MAP SCORES TO FUNCTIONS
+
+// TODO: Abstract the scores into their functions
+
+// WRITE BEATNIKIFY ALGORITHM
+
+// TODO: Create method beatnikify() using algorithm that takes a char and produces the Beatnik program to print that char
+
+// INTEGRATION
+
+// TODO: Read html input to var
+// TODO: Parse each char from the input to a number value in an array of ints (scores corresponding to that character)
+// TODO: Run each char in the array through the beatnikify() method, appending the output to a writer stream
+// TODO: Write the value of the writer stream to a string
+// TODO: Output the string to the html page
